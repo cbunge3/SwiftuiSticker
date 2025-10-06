6ef9e8b1cfabc6c1142d746205a595a5dec8b55a
cbunge3 <59035777+cbunge3@users.noreply.github.com>
2025-10-06 10:06:55 -0500

    Update StickerPatternType.swift

--- Sticker/StickerPatternType.swift
+++ Sticker/StickerPatternType.swift
@@ -6,10 +6,7 @@
 //
 
 
-// StickerPatternType.swift
-// Sticker
-//
-// Created by Benjamin Pisano on 26/01/2025.
+
 //
 import Foundation
 public enum StickerPatternType: Int, Hashable, Equatable, Sendable {
